524fb947cb522787a10ccd810fd8578fb8ac8756
David  Kang <david2004kang@gmail.com>
2020-03-23 14:41:24 +0800

    Update code, added gain.txt for setting

--- YAV_ADIO_GUI.py
+++ YAV_ADIO_GUI.py
@@ -14,12 +14,17 @@ from matplotlib.figure import Figure
 import ctypes
 import datetime
 import numpy as np
-
-
 __author__ = 'David Kang'
 import numpy as np
 from Tkinter import *
 import matplotlib.pyplot as plt
+import os
+
+setting = [1] * 10
+if os.path.isfile(os.getcwd() + r'\gain.txt'):
+    with open(os.getcwd() + r'\gain.txt', "rt") as file_handler:
+        setting = file_handler.read().strip().split('\n')
+    setting = map(lambda x: eval(x.split('#')[0].strip()), setting)
 
 
 class ADIO_dll_wrapper:
@@ -90,7 +95,7 @@ class YAV_ADIO_GUI:
         self.fig_voltage2_array = np.array([0] * len(self.fig_time_array))
         self.fig_current_array = np.array([0] * len(self.fig_time_array))
         self.subplot1 = self.fig.add_subplot(211)
-        self.subplot1.set_ylim(bottom=0.0, top=25.0)
+        self.subplot1.set_ylim(bottom=-2.0, top=30.0)
         self.subplot1.set_xlabel("time(Sec.)")
         self.subplot1.set_ylabel("voltage(V)")
         self.subplot1.text(10, 10, "Vol1:{} V \nVol2:{} V".format(self.fig_voltage1_array[-1],
@@ -99,7 +104,7 @@ class YAV_ADIO_GUI:
         self.subplot2 = self.fig.add_subplot(212, sharex=self.subplot1)
         self.subplot2.set_xlabel("time(Sec.)")
         self.subplot2.set_ylabel("current(mA)")
-        self.subplot2.set_ylim(bottom=0.0, top=2000.0)
+        self.subplot2.set_ylim(bottom=-100.0, top=5000.0)
         self.subplot2.plot(self.fig_time_array, self.fig_voltage1_array, color='red',
                            linewidth=1.0, linestyle='--', label="current(mA)")
         self.fig.legend()
@@ -133,25 +138,32 @@ class YAV_ADIO_GUI:
         return np.array(_temp_list)
 
     def animate(self, _index):
+        global setting
         if self._data_source is None:
             return
         _temp_val1 = self._data_source.value_1
         _temp_val2 = self._data_source.value_2
+        _voltage = max([_temp_val1, _temp_val2]) * 50.0 * setting[0] / 4096
+        _current = abs(_temp_val1 - _temp_val2) * 100000 * setting[1] / 4096 + setting[2]
+        if _current < 0.0:
+            _current = 0.0
+        print "_temp_val1: {}, _temp_val2: {}, abs(A-B)= {}"\
+            .format(
+                _temp_val1, _temp_val2,
+                abs(_temp_val2 - _temp_val1))
         self.fig_time_array = self.array_pop_push(self.fig_time_array, self.fig_time_array[-1] + 1)
-        self.fig_voltage1_array = self.array_pop_push(self.fig_voltage1_array, _temp_val1 * 50.0 / 4096)
-        self.fig_voltage2_array = self.array_pop_push(self.fig_voltage2_array, _temp_val2 * 50.0 / 4096)
-        self.l1['text'] = "Voltage: {} V, Current: {} A, Time: {}".format(
-            _temp_val1 * 50.0 / 4096,
-            abs(_temp_val1 - _temp_val2) * 5000.0 / 4096,
-            str(datetime.datetime.now() - self.start_time)
+        self.fig_voltage1_array = self.array_pop_push(self.fig_voltage1_array, _voltage)
+        self.fig_voltage2_array = self.array_pop_push(self.fig_voltage2_array, _current)
+        self.l1['text'] = "Voltage: {} V, Current: {} mA, Time: {}".format(
+            _voltage, _current, str(datetime.datetime.now() - self.start_time)
         )
 
         self.subplot2.clear()
         self.subplot1.clear()
-        self.subplot2.set_ylim(bottom=0.0, top=2000.0)
-        self.subplot2.plot(self.fig_time_array, self.fig_voltage1_array, color='red',
+        self.subplot2.set_ylim(bottom=-100.0, top=5000.0)
+        self.subplot2.plot(self.fig_time_array, self.fig_voltage2_array, color='red',
                            linewidth=1.0, linestyle='--', label="voltage 2")
-        self.subplot1.set_ylim(bottom=0.0, top=25.0)
+        self.subplot1.set_ylim(bottom=-2.0, top=30.0)
         self.subplot1.plot(self.fig_time_array, self.fig_voltage1_array)
         # self.subplot1.plot(self.fig_time_array, self.fig_voltage2_array, color='red',
         #                    linewidth=1.0, linestyle='--', label="voltage 2")
@@ -162,14 +174,15 @@ if __name__ == "__main__":
 
     process_counter = 0
     process_name = psutil.Process(os.getpid()).name()
-    for _process in psutil.process_iter():
-        try:
-            if process_name.lower() in _process.name().lower():
-                process_counter += 1
-                if process_counter >= 2:
-                    sys.exit()
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+    if process_name != "python.exe":
+        for _process in psutil.process_iter():
+            try:
+                if process_name.lower() in _process.name().lower():
+                    process_counter += 1
+                    if process_counter >= 2:
+                        sys.exit()
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
 
     try:
         gui_handle = YAV_ADIO_GUI()
